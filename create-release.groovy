5b28c9f31cbf40dfbdf71212c99aa01301275cfd
Grigorii Tkachuk <tkgreg@me.com>
2017-01-11 14:17:31 +0100

    Let's try simple one

--- create-release.groovy
+++ create-release.groovy
@@ -1,25 +1,24 @@
 xlr {
-  release("Full blown release") {
-    description "Sample template created from Groovy DSL"
-    variables {
-      stringVariable("testVar")
-    }
-      phase("Example") {
-        tasks {
-          notification('notification task') {
-            description "notification task"
-            addresses "mail1@mail.com", "mail2@mail.com"
-            subject "my subject"
-            body "my notification body"
-          }
-          userInput('userInput task') {
-            description "userInput task"
-            variables {
-              variable("testVar")
-            }
-          }
+    release("Full blown release") {
+        description "Sample template created from Groovy DSL"
+        variables {
+            stringVariable("testVar")
         }
-      }
+        phase("Example") {
+            tasks {
+                notification('notification task') {
+                    description "notification task"
+                    addresses "mail1@mail.com", "mail2@mail.com"
+                    subject "my subject"
+                    body "my notification body"
+                }
+                userInput('userInput task') {
+                    description "userInput task"
+                    variables {
+                        variable("testVar")
+                    }
+                }
+            }
+        }
     }
-  }
 }
