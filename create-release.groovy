502abaa8432bae69353b08bf4beb0ceee30bfbb3
Grigorii Tkachuk <tkgreg@me.com>
2017-01-11 14:28:56 +0100

    Let's try simple one

--- create-release.groovy
+++ create-release.groovy
@@ -4,18 +4,20 @@ xlr {
         variables {
             stringVariable("testVar")
         }
-        phase("Example") {
-            tasks {
-                notification('notification task') {
-                    description "notification task"
-                    addresses "mail1@mail.com", "mail2@mail.com"
-                    subject "my subject"
-                    body "my notification body"
-                }
-                userInput('userInput task') {
-                    description "userInput task"
-                    variables {
-                        variable("testVar")
+        phases {
+            phase("Example") {
+                tasks {
+                    notification('notification task') {
+                        description "notification task"
+                        addresses "mail1@mail.com", "mail2@mail.com"
+                        subject "my subject"
+                        body "my notification body"
+                    }
+                    userInput('userInput task') {
+                        description "userInput task"
+                        variables {
+                            variable("testVar")
+                        }
                     }
                 }
             }
