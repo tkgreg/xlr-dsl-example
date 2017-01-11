73f9da7f4903b573b6942360b2eeab4e173a3c16
Grigorii Tkachuk <tkgreg@me.com>
2017-01-11 14:02:24 +0100

    create release example

---
+++ create-release.groovy
@@ -0,0 +1,92 @@
+xlr {
+  release("Full blown release") {
+    description "Sample template created from Groovy DSL"
+    variables {
+      stringVariable("testVar")
+    }
+    phases {
+      phase("First round") {
+        tasks {
+          task {
+            type "xlrelease.ScriptTask"
+            title "script task"
+            description "dynamic script task"
+            script "println 'hello world'"
+          }
+          manual("manual task") {
+            description "manual task"
+          }
+          createRelease('createRelease task') {
+            description "createRelease task"
+            newReleaseTitle "title of the new release"
+            startRelease false
+            template "Container folder/Sub folder/Template 1" // can be path OR Id
+            releaseTags "tag1", "tag2", "tag2"
+          }
+          gate('gate task') {
+            description 'gate task'
+            conditions {
+              condition("my condition 1")
+              condition("my condition 2")
+            }
+            dependencies {
+              dependency {
+                variable 'customVar' // NOTE : Do not use dollar{} syntax here as it would be interpolated by XLR
+              }
+              dependency {
+                target {
+                  release "my release"
+                  phase "my phase"
+                  task "my task"
+                }
+              }
+            }
+          }
+        }
+      }
+      phase("second round") {
+        tasks {
+          notification('notification task') {
+            description "notification task"
+            addresses "mail1@mail.com", "mail2@mail.com"
+            subject "my subject"
+            body "my notification body"
+          }
+          userInput('userInput task') {
+            description "userInput task"
+            variables {
+              variable("testVar")
+            }
+          }
+        }
+      }
+      phase {
+        title "third round"
+        tasks {
+          parallelGroup('pg') {
+            tasks {
+              manual("manual task 1") {
+                description "manual task 1"
+              }
+              script("script task 1") {
+                description "script task 1"
+                script "println 'hello world'"
+              }
+            }
+          }
+          sequentialGroup('sg') {
+            tasks {
+              groovyScript("groovyScript task 1") {
+                description "groovyScript task 1"
+                script 'println "hello world"'
+              }
+              manual("manual task 2") {
+                description "manual task 2"
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
